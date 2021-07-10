405117d9a56ca8e98fe0ee0d288cfd82114429a0
raphavmendes <86535607+raphavmendes@users.noreply.github.com>
2021-07-09 21:29:34 -0300

    Delete convert.js

--- convert.js
+++
@@ -1,8 +0,0 @@
-// Convert Celsius to Fahrenheit
-
-function convertToFahrenheit (value) {
-    return value * 1.8 + 32
-}
-
-let result = convertToFahrenheit(40)
-    console.log(`O Valor em Fahrenheit é ${result}`)
